bf0e81965f9fb1778cb902d9a106e8b08d7edfd6
Alex Arias <alex.arias.jorym@gmail.com>
2022-05-03 12:05:43 +0800

    progressbar loading added

--- app/src/main/java/com/example/tradedocqst/MainActivity.java
+++ app/src/main/java/com/example/tradedocqst/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.tradedocqst;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        progressBar = findViewById(R.id.progress_bar);
+        textView = findViewById(R.id.textView);
+
+        progressBar.setMax(100);
+        progressBar.setScaleY(3f);
+
+        progressAnimation();
+    }
+
+    public void progressAnimation(){
+        ProgressBarAnimation anim = new ProgressBarAnimation( this, progressBar, textView, 0f, 100f);
+        anim.setDuration(8000);
+        progressBar.setAnimation(anim);
     }
 }
--- app/src/main/java/com/example/tradedocqst/ProgressBarAnimation.java
+++ app/src/main/java/com/example/tradedocqst/ProgressBarAnimation.java
@@ -1,4 +1,40 @@
 package com.example.tradedocqst;
 
-public class ProgressBarAnimation {
+import android.content.Context;
+import android.content.Intent;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class ProgressBarAnimation extends Animation {
+
+    private Context context;
+    private ProgressBar progressBar;
+    private TextView textView;
+    private float from;
+    private float to;
+
+    public ProgressBarAnimation(Context context, ProgressBar progressBar, TextView textView, float from, float to){
+        this.context = context;
+        this.progressBar = progressBar;
+        this.textView = textView;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t){
+        super.applyTransformation(interpolatedTime, t);
+        float value = from + (to - from) * interpolatedTime;
+        progressBar.setProgress((int)value);
+        String loadnum = String.format(Locale.US,"%1f %%", value);
+        textView.setText(loadnum);
+
+        if (value == to){
+            context.startActivity(new Intent(context, LoginActivity.class));
+        }
+    }
 }
