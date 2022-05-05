1b48dfe061726edf92dbb2ae1c8a8169a465ce94
Alex Arias <alex.arias.jorym@gmail.com>
2022-05-05 20:24:36 +0800

    added user button functionalities

--- app/src/main/java/com/grouptwo/tradedocqst/LoginActivity.java
+++ app/src/main/java/com/grouptwo/tradedocqst/LoginActivity.java
@@ -2,7 +2,11 @@ package com.grouptwo.tradedocqst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.MessageFormat;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Intent intent = getIntent();
+        String value = intent.getStringExtra("user_group"); //if it's a string you stored.
+
+        TextView txtUsrLogin = (TextView) findViewById(R.id.txtUsrLogin);
+        txtUsrLogin.setText(MessageFormat.format("{0}\nLogin", value));
     }
 }
--- app/src/main/java/com/grouptwo/tradedocqst/UserLoginActivity.java
+++ app/src/main/java/com/grouptwo/tradedocqst/UserLoginActivity.java
@@ -2,13 +2,51 @@ package com.grouptwo.tradedocqst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class UserLoginActivity extends AppCompatActivity {
+public class UserLoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    // creating the user buttons
+    Button btnStudent, btnTeacher, btnAdmin;
+
+    public void btnUser(String user){
+        Intent myIntent = new Intent(UserLoginActivity.this, LoginActivity.class);
+
+        myIntent.putExtra("user_group", user); // Parameters
+        UserLoginActivity.this.startActivity(myIntent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ulogin);
+
+        // connecting buttons
+        btnStudent = findViewById(R.id.btnStudent);
+        btnTeacher = findViewById(R.id.btnTeacher);
+        btnAdmin = findViewById(R.id.btnAdmin);
+
+        // apply onClickListener over buttons
+        btnStudent.setOnClickListener(this);
+        btnTeacher.setOnClickListener(this);
+        btnAdmin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        int id = v.getId();
+        if(id == R.id.btnStudent) {
+            btnUser("Student");
+        }
+        else if(id == R.id.btnTeacher) {
+            btnUser("Teacher");
+        }
+        else if(id == R.id.btnAdmin) {
+            btnUser("Admin");
+        }
     }
 }
