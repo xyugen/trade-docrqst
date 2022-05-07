5102ad6f0afe109c356100b85f068150230cdb55
Alex Arias <alex.arias.jorym@gmail.com>
2022-05-07 17:50:01 +0800

    sign up activity buttons

--- app/src/main/java/com/grouptwo/tradedocqst/SignUpActivity.java
+++ app/src/main/java/com/grouptwo/tradedocqst/SignUpActivity.java
@@ -2,13 +2,49 @@ package com.grouptwo.tradedocqst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // Creating buttons
+    Button btnSUNext, btnBack, btnForgotPW;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // connecting buttons
+        btnSUNext = findViewById(R.id.btnSUNext);
+        btnBack = findViewById(R.id.btnBack);
+        btnForgotPW = findViewById(R.id.btnForgotPasswd);
+
+        // apply onClick Listener
+        btnSUNext.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+        btnForgotPW.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.btnSUNext) {
+            Intent intent = new Intent(this, SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else if(id == R.id.btnBack) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else if(id == R.id.btnForgotPasswd) {
+            Toast toast = Toast.makeText(getApplicationContext(), "Clicked forgot pass!", Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
