98af64b8f9cd28679bbb60b68fc0a4e4a4d80ed5
Alex Arias <alex.arias.jorym@gmail.com>
2022-06-17 06:45:49 +0800

    request activity functionalities

--- app/src/main/java/com/grouptwo/tradedocqst/request/RequestActivity.java
+++ app/src/main/java/com/grouptwo/tradedocqst/request/RequestActivity.java
@@ -1,5 +1,6 @@
 package com.grouptwo.tradedocqst.request;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -10,14 +11,25 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.grouptwo.tradedocqst.R;
+import com.grouptwo.tradedocqst.login.SignUpActivity;
 import com.grouptwo.tradedocqst.users.StudentActivity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class RequestActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -25,9 +37,11 @@ public class RequestActivity extends AppCompatActivity implements View.OnClickLi
     TextView txtLRN, txtFullName, txtSection, txtDocuments;
     Button btnRqstRequest, btnRqstCancel;
     ArrayList<String> documents;
+    Object[] docsArray;
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     FirebaseUser fUser;
+    String currentDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +55,8 @@ public class RequestActivity extends AppCompatActivity implements View.OnClickLi
         txtDocuments = findViewById(R.id.txtDocsRqstdCon);
         btnRqstRequest = findViewById(R.id.btnRqstRequest);
         btnRqstCancel = findViewById(R.id.btnRqstCancel);
+
+        currentDate = java.text.DateFormat.getDateInstance().format(new Date());
 
         // firebase
         fAuth = FirebaseAuth.getInstance();
@@ -86,11 +102,41 @@ public class RequestActivity extends AppCompatActivity implements View.OnClickLi
     public void onClick(View view) {
         int id = view.getId();
         if (id == R.id.btnRqstRequest){
-            return;
+            requestDocuments();
         }
         else if (id == R.id.btnRqstCancel){
             startActivity(new Intent(getApplicationContext(), StudentActivity.class));
             finish();
         }
+    }
+
+    private void requestDocuments() {
+        assert fUser != null;
+        DocumentReference df = fStore.collection("Users").document(fUser.getUid()).collection("Requests").document();
+
+        Map<String, Object> docRequest = new HashMap<>();
+        docRequest.put("FullName", Objects.requireNonNull(txtFullName.getText()).toString().trim());
+        docRequest.put("Section", txtSection.getText().toString().trim());
+        docRequest.put("LRN", Objects.requireNonNull(txtLRN.getText()).toString().trim());
+
+        // put in documents array
+        docsArray = documents.toArray();
+        docRequest.put("Documents", Arrays.asList(docsArray));
+
+
+        // date requested
+        docRequest.put("RequestDate", currentDate);
+
+        // queued
+        docRequest.put("Done", 0);
+        Log.d("Requested", "Documents" + documents);
+        Log.d("RequestDate", "Date" + currentDate);
+
+        df.set(docRequest)
+                .addOnSuccessListener(unused -> Toast.makeText(getApplicationContext(), "Document/s request success", Toast.LENGTH_SHORT).show())
+                .addOnFailureListener(e -> Toast.makeText(getApplicationContext(), "Document/s request failed", Toast.LENGTH_SHORT).show());
+
+        startActivity(new Intent(getApplicationContext(), StudentActivity.class));
+        finish();
     }
 }
